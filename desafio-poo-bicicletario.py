9ffdf3f2a3699d96c7d5df0bec9e792f97f786c0
Ana Beatriz <anabeatriz.arruda7@gmail.com>
2026-04-07 21:18:03 -0300

    add: estudos poo - exercicio 1

---
+++ desafio-poo-bicicletario.py
@@ -0,0 +1,35 @@
+class Bicicleta:
+    def __init__(self, cor, modelo, ano, valor):     #self ou this
+        self.cor = cor
+        self.modelo = modelo
+        self.ano = ano
+        self.valor = valor
+
+    #Métodos:
+    def buzinar(self):
+        print("plim plim")
+
+    def parar(self):
+        print("parando bicicleta")
+        print("parou!")
+
+    def correr(self):
+        print("Vruummm")
+
+
+
+    def __str__(self):
+        return f"bicicleta: cor={self.cor}, modelo={self.modelo}, ano={self.ano}, valor={self.valor}"
+    
+
+#criar a instancia para a classe:
+b1 = Bicicleta("vermelha", "caloi", 2022, 600)
+print(b1)
+
+b1.buzinar()
+b1.correr()
+b1.parar()
+
+# Bicicleta.parar(b1)
+# Bicicleta.correr(b1)
+# Bicicleta.buzinar(b1)
