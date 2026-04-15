6a06135bce16614209eb10cc58cc5e9e0f6c36f2
Ana-Btrzz <anabeatriz.arruda7@gmail.com>
2026-04-15 17:36:12 -0300

    add: exemplo de polimorfismo

---
+++ polimorfismo.py
@@ -0,0 +1,21 @@
+class Passaro:
+    def voar(self):
+        print("Voando")
+
+
+class Pardal(Passaro):
+    def voar(self):
+        print("Pardal pode voar!")
+
+
+class Avestruz(Passaro):
+    def voar(self):
+        print("Avestruz não voa")
+
+
+def plano_voo(obj):
+        obj.voar()
+
+
+plano_voo(Pardal())
+plano_voo(Avestruz())
